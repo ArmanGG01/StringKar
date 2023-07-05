cc8c28676f44a4d08d51259ca1c600d6033a7340
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-07-06 00:50:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,34 @@
-from os import getenv
-from dotenv import load_dotenv
+import Config
+import logging
+from pyrogram import Client, idle
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
-load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+app = Client(
+    ":memory:",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="StringSessionBot"),
+)
+
+
+# Run Bot
+if __name__ == "__main__":
+    try:
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = app.get_me().username
+    print(f"@{uname} Started Successfully!")
+    idle()
+    app.stop()
+    print("Bot stopped. Alvida!")
